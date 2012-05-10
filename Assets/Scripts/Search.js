46c7645a39ccb603fba610c1c17d98f8de6e5573
Kohichi Aoki <drikin@gmail.com>
2012-05-10 08:19:45 -0700

    UI scale test

--- Assets/Scripts/Search.js
+++ Assets/Scripts/Search.js
@@ -11,6 +11,9 @@ function Update () {
 }
 
 function OnGUI(){
+    var scale = 1.5;
+    var scaledMatrix: Matrix4x4 = Matrix4x4.identity.Scale(Vector3(scale,scale,scale));
+    GUI.matrix = scaledMatrix;
     keyword = GUI.TextField(Rect(Screen.width/2 - 100, Screen.height/2, 200, 20), keyword, 25);
     if( GUI.Button(Rect(Screen.width/2 - 50, Screen.height/2 + 30, 100, 25), "Search")) {
         print(keyword);
