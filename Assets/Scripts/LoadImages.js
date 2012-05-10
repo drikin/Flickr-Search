95929602421cecff6ad45a4088b54bd6113126ca
Kohichi Aoki <drikin@gmail.com>
2012-05-10 08:19:27 -0700

    change image size from n to z

--- Assets/Scripts/LoadImages.js
+++ Assets/Scripts/LoadImages.js
@@ -7,7 +7,7 @@ import System.Collections.Hashtable;
 //private var json_url = "http://api.flickr.com/services/feeds/photos_public.gne?id=34202117%40N00&format=json&nojsoncallback=1";
 //private var json_url = "http://api.flickr.com/services/feeds/photos_public.gne?id=26153219%40N00&format=json&nojsoncallback=1";
 //private var json_url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=masakiishitani&format=json&nojsoncallback=1";
-private var json_url_base = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=124f163e85a1ad89419349ba560854b0&format=json&nojsoncallback=1&extras=url_n&text=";
+private var json_url_base = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=124f163e85a1ad89419349ba560854b0&format=json&nojsoncallback=1&extras=url_z&text=";
 //private var keywoard = WWW.EscapeURL("佐々木希");
 
 function Start () {
@@ -46,7 +46,7 @@ function changeImg(images: ArrayList) {
         //var media: Hashtable = image["media"];
         //var url: String = media["m"];
         //url = url.Replace("_m.jpg", ".jpg");
-        var url: String = image["url_n"];
+        var url: String = image["url_z"];
         print(url);
 
         var www: WWW  = new WWW(url);
